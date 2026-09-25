428c0ef215bee9069e1cbf08ed881c54b8da0a65
DomenicaPugo11 <evlyn562@gmail.com>
2026-09-25 18:23:01 -0500

    Calcule el valor disponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,10 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+ function calcularDisponible(ingresos,egresos){
+    let valorDisponible=ingresos-egresos;
+    if (valorDisponible<0){
+        return 0;
+    }else{
+        return valorDisponible;
+    }
+ }
--- simulador.js
+++ simulador.js
@@ -1,1 +1,15 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular(){
+    let ctxIngresos=document.getElementById("txtIngresos");
+    let ingresosStr=ctxIngresos.value;
+    let ingresos=parseFloat(ingresosStr);
+
+    let ctxEgresos=document.getElementById("txtEgresos");
+    let egresosStr=ctxEgresos.value;
+    let egresos=parseFloat(egresosStr);
+
+    let valorDisponible=calcularDisponible(ingresos,egresos);
+    let spValorDisponible=document.getElementById("spnDisponible");
+    spValorDisponible.textContent=valorDisponible;
+}
